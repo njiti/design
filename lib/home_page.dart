c27b2068f12b33432a77c08ad3d2a0c8d49b1397
njiti <kamaukan77@gmail.com>
2023-05-23 18:29:17 +0300

    Initial commit

---
+++ lib/home_page.dart
@@ -0,0 +1,23 @@
+import 'package:design/Learn_flutter_page.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: ElevatedButton(
+        onPressed: (){
+          Navigator.of(context).push(
+            MaterialPageRoute(builder: (BuildContext context){
+              return const LearnFlutterPage();
+          },
+            ),
+          );
+        },
+        child: const Text('Learn Flutter'),
+      ),
+    );
+  }
+}
